8aa6fccbff066def351f44e157018614537c4ad3
ev-genii-r <ev_genii_r@mail.ru>
2025-11-06 13:33:07 +0300

    v0.1-not-compleated

--- README.md
+++
@@ -1,2 +0,0 @@
-# authentication-service
-for innowise
---
+++ src/main/java/com/innowise/evgenii/service/UserService.java
@@ -0,0 +1,39 @@
+package com.innowise.evgenii.service;
+
+import com.innowise.evgenii.entity.Role;
+import com.innowise.evgenii.entity.User;
+import com.innowise.evgenii.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(String email, String password, Role role) {
+
+        if (userRepository.existsByEmail(email)) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRole(role);
+
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+}
